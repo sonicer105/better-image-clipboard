45395eac28fc52253c10a75c11398ccf47527493
LinuxPony <sonic.ert@gmail.com>
2023-02-05 02:16:23 -0800

    - Added category sorting and renaming - Added hover and selected highlighting - Added support for importing slightly malformed LZW compressed backups - Miscellaneous changes - Bumped version to 1.3.0

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
+"use strict"
+
 window.appState = {
     editing: false,
     data: {},
     targetIndex: 0,
     targetGroup: 'Uncategorized'
 }
+
+//#region Local Storage
 
 function lpMaybeSaveToLocalStorage(){
     if(!!localStorage){
@@ -23,6 +27,10 @@ function lpMaybeReadFromLocalStorage(){
     window.appState.data = {};
     return false;
 }
+
+//#endregion
+
+//#region Add Image
 
 function lpAddImage(){
     let urlEl = $('#url');
@@ -46,6 +54,10 @@ function lpAddImage(){
     return false;
 }
 
+//#endregion
+
+//#region Render Images / Categories
+
 function lpDrawImages(){
     let container = $('main');
     container.empty();
@@ -53,28 +65,57 @@ function lpDrawImages(){
         container.append($('<section class="icon-group"><h2>No Icons Yet. Click <kbd>Edit</kbd> and add one.</h2></section>'));
         return;
     }
+    container.append($('<p id="instructions-no-edit">Click on an image to copy it\'s URL to the clipboard.</p>'));
+    container.append($('<p id="instructions-edit">Click on an image to edit it.</p>'));
+    let index = 0;
     for (let keypair of Object.entries(window.appState.data)){
-        let section = $(`<section class="icon-group"><h2>${keypair[0]}</h2></section>`);
+        let safeText = escapeHtml(keypair[0]);
+        let section = $(`<section class="icon-group">
+<h2><span class="cat-title">${safeText}</span>
+<label><input type="text" class="rename-cat" id="rename-${index}"></label>
+<span class="rename-cat-wrapper cat-controls">
+    <a href="#"><span class="sr-only">Rename</span><i class="far fa-square-arrow-right fa-fw" aria-hidden="true"></i></a>
+</span>
+<span class="move-cat-up-wrapper cat-controls">
+    <a href="#"><span class="sr-only">Move Forward</span><i class="far fa-square-caret-up fa-fw" aria-hidden="true"></i></a>
+</span>
+<span class="move-cat-down-wrapper cat-controls">
+    <a href="#"><span class="sr-only">Move Backwards</span><i class="far fa-square-caret-down fa-fw" aria-hidden="true"></i></a>
+</span>
+</h2>
+</section>`);
+        section.find('a').data('target', keypair[0]);
+        section.find('.rename-cat-wrapper a, input.rename-cat').val(keypair[0]).data('target', 'rename-' + index).data('old', keypair[0]);
         let images = $('<div class="icons"></div>')
         for (let img of Object.entries(keypair[1])){
-            let imgEl = $(`<img src="${img[1]}" data-index="${img[0]}" data-group="${keypair[0]}">`);
+            let imgEl = $(`<img src="${img[1]}" data-index="${img[0]}" data-group="${keypair[0]}" alt="An emote or sticker">`);
             imgEl.on('click', lpHandleImageClick);
             images.append(imgEl);
         }
         section.append(images);
         container.append(section);
+        index++;
     }
-    let groups = $('#group-list')
+    $("input.rename-cat").on("keypress", lpRenameCatKbd);
+    $(".rename-cat-wrapper a").on("click", lpRenameCat);
+    $(".move-cat-up-wrapper a").on("click", lpMoveCatUp);
+    $(".move-cat-down-wrapper a").on("click", lpMoveCatDown);
+    let groups = $('#group-list');
     groups.empty();
     for(let group of Object.keys(window.appState.data)){
         groups.append(`<option value="${group}">${group}</option>`);
     }
 }
 
+//#endregion
+
+//#region Activate/Deactivate Editing
+
 function lpOpenEditing(){
     window.appState.editing = true;
     $('.edit-tool').show();
     $('.non-edit-tool').hide();
+    $(document.body).addClass('is-editing');
     return false;
 }
 
@@ -82,11 +123,18 @@ function lpCloseEditing(){
     window.appState.editing = false;
     $('.edit-tool, .selection-tool').hide();
     $('.non-edit-tool, .non-selection-tool').show();
+    $(document.body).removeClass('is-editing');
     return false;
 }
 
+//#endregion
+
+//#region Active Image Handling
+
 function lpHandleImageClick(e){
     let target = $(e.target);
+    $('.icon-group img.active').removeClass('active');
+    $(this).addClass('active');
     if(window.appState.editing){
         window.appState.targetIndex = target.data('index');
         window.appState.targetGroup = target.data('group');
@@ -158,12 +206,79 @@ function lpMoveDown(){
     return false;
 }
 
+//#endregion
+
+//#region Category Editing
+
+function lpRenameCatKbd(e) {
+    if(e.key === "Enter") {
+        e.preventDefault();
+        (lpRenameCat.bind(this))()
+    }
+}
+
+function lpRenameCat() {
+    let newCatName = $("#" + $(this).data('target')).val();
+    let oldCatName = $(this).data('old');
+    if(newCatName === oldCatName) return false;
+    let dataKeys = Object.keys(window.appState.data);
+    let targetIndex = dataKeys.indexOf(oldCatName);
+    dataKeys[targetIndex] = newCatName;
+    window.appState.data[newCatName] = window.appState.data[oldCatName];
+    delete window.appState.data[oldCatName];
+    lpSetNewOrder(dataKeys);
+    toastSuccess("Category renamed successfully!")
+    return false;
+}
+
+function lpMoveCatDown() {
+    let target = $(this).data('target');
+    if(!target) return false;
+    let dataKeys = Object.keys(window.appState.data);
+    let targetIndex = dataKeys.indexOf(target);
+    if(targetIndex < 0) return false;
+    lpArrayIndexMove(dataKeys, targetIndex, targetIndex + 1);
+    lpSetNewOrder(dataKeys);
+    return false;
+}
+
+function lpMoveCatUp() {
+    let target = $(this).data('target');
+    if(!target) return false;
+    let dataKeys = Object.keys(window.appState.data);
+    let targetIndex = dataKeys.indexOf(target);
+    if(targetIndex < 0) return false;
+    lpArrayIndexMove(dataKeys, targetIndex, targetIndex - 1);
+    lpSetNewOrder(dataKeys);
+    return false;
+}
+
+function lpSetNewOrder(orderKeys) {
+    let newData = {};
+    for(let i in orderKeys) {
+        if(orderKeys.hasOwnProperty(i)) {
+            newData[orderKeys[i]] = window.appState.data[orderKeys[i]];
+        }
+    }
+    window.appState.data = newData;
+    lpMaybeSaveToLocalStorage();
+    lpDrawImages();
+}
+
+//#endregion
+
+//#region Array Tools
+
 function lpArrayIndexMove(arr, old_index, new_index) {
     if (old_index < 0 || new_index < 0) return false;
     if (new_index >= arr.length) return false;
     arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
     return true;
 }
+
+//endregion
+
+//#region Menus
 
 function lpShowOptionsMenu(){
     $('#options-model').addClass('active');
@@ -186,6 +301,10 @@ function lpHideBackupMenu(){
     $('#options-model').addClass('active');
     return false;
 }
+
+//#endregion
+
+//#region Backups
 
 function lpExportData(e){
     let targetType = $(e.target).data('target') ?? 'file';
@@ -226,7 +345,9 @@ function lpOpenImportFile(){
                 lpImportData(JSON.parse(fr.result))
                 toastSuccess("JSON items imported!");
             } catch (e) {
-                lpImportData(JSON.parse(LZW.decompress(JSON.parse(atob(fr.result)))))
+                let temp = atob(fr.result);
+                if(temp.indexOf('[') < 0) temp = '[' + temp + ']';
+                lpImportData(JSON.parse(LZW.decompress(JSON.parse(temp))));
                 toastSuccess("LZW Items imported!");
             }
         }
@@ -241,7 +362,9 @@ function lpParseImportField(){
         lpImportData(JSON.parse(fieldVal))
         toastSuccess("JSON items imported!");
     } catch (e) {
-        lpImportData(JSON.parse(LZW.decompress(JSON.parse(atob(fieldVal)))))
+        let temp = atob(fieldVal);
+        if(temp.indexOf('[') < 0) temp = '[' + temp + ']';
+        lpImportData(JSON.parse(LZW.decompress(JSON.parse(temp))))
         toastSuccess("LZW Items imported!");
     }
     return false;
@@ -258,6 +381,10 @@ function lpImportData(jsonData){
     $('#file-import').val(null);
 }
 
+//#endregion
+
+//#region Delete All
+
 function lpDeleteAll() {
     localStorage.removeItem('imageData');
     window.appState.targetGroup = 'Uncategorized';
@@ -270,10 +397,18 @@ function lpDeleteAll() {
     return false;
 }
 
+//#endregion
+
+//#region Dedupe
+
 function lpDedupe() {
     toastError("Not Yet Implemented! SORRY!");
     return false;
 }
+
+//#endregion
+
+//#region Toasts
 
 var toastTimeout = null;
 
@@ -302,6 +437,10 @@ function toastSuccess(text) {
 function resetToast() {
     $("#toast-model").removeClass('active');
 }
+
+//#endregion
+
+//#region Init
 
 lpMaybeReadFromLocalStorage();
 
@@ -345,6 +484,22 @@ jQuery(function ($){
     lpDrawImages();
 })
 
+//#endregion
+
+//#region Escape HTML
+
+function escapeHtml(unsafe) {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+//#endregion
+
+//#region LZW Compression
 
 //LZW Compression/Decompression for Strings
 //SOURCE: https://rosettacode.org/wiki/LZW_compression#JavaScript
@@ -430,3 +585,5 @@ var LZW = {
         return result;
     }
 }
+
+//#endregion
